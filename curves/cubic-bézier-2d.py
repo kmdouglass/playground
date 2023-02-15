d4b1f3940b633b0ea6ff9b83b0873dc2862453de
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2023-02-15 18:07:33 +0100

    Add matrices for acceleration and jerk

--- curves/cubic-bézier-2d.py
+++ curves/cubic-bézier-2d.py
@@ -1,15 +1,20 @@
 """Create a 2D cubic Bézier curve from control points."""
 
+from enum import Enum
+
 import matplotlib.pyplot as plt
 import numpy as np
 import numpy.typing as npt
 
 
-POS = 0
-VEL = 1
+class Property(Enum):
+    POSITION = 0
+    VELOCITY = 1
+    ACCELERATION = 2
+    JERK = 3
 
 
-# first dimension corresponds to position, velocity
+# first dimension corresponds to position, velocity, acceleration, and jerk
 CHAR_MATRIX = np.array(
     [[[1, 0, 0, 0],
       [-3, 3, 0, 0],
@@ -19,23 +24,30 @@ CHAR_MATRIX = np.array(
      [[-3, 3, 0, 0],
       [6, -12, 6, 0],
       [-3, 9, -9, 3],
+      [0, 0, 0, 0]],
+      
+     [[6, -12, 6, 0],
+      [-6, 18, -18, 6],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]],
+      
+     [[-6, 18, -18, 6],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
       [0, 0, 0, 0]]],
     dtype=np.float64,
 )
 
 
-def curve(times: npt.ArrayLike, ctrl_pts: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+def bezier(
+    times: npt.ArrayLike,
+    ctrl_pts: npt.NDArray[np.float64],
+    property: Property = Property.POSITION,
+) -> npt.NDArray[np.float64]:
     t = np.asanyarray(times)
     powers = np.power(t, [0, 1, 2, 3])
 
-    return np.linalg.multi_dot((powers, CHAR_MATRIX[POS], ctrl_pts))
-
-
-def velocity(times: npt.ArrayLike, ctrl_pts: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
-    t = np.asanyarray(times)
-    powers = np.power(t, [0, 1, 2, 3])
-
-    return np.linalg.multi_dot((powers, CHAR_MATRIX[VEL], ctrl_pts))
+    return np.linalg.multi_dot((powers, CHAR_MATRIX[property.value], ctrl_pts))
     
 
 if __name__ == "__main__":
@@ -52,7 +64,7 @@ if __name__ == "__main__":
     # Make t into a (num_time_points, 1) array
     t_reshaped = t[:, np.newaxis]
     
-    B = curve(t_reshaped, control_points)
+    B = bezier(t_reshaped, control_points)
     
 
     plt.plot(control_points[:, 0], control_points[:, 1], "o")
@@ -61,8 +73,8 @@ if __name__ == "__main__":
     # Plot velocity at specific times
     t_vels = np.linspace(0, 1, 5)
     for t_vel in t_vels:
-        pos = curve(t_vel, control_points)
-        vel = velocity(t_vel, control_points)
+        pos = bezier(t_vel, control_points)
+        vel = bezier(t_vel, control_points, Property.VELOCITY)
         plt.arrow(pos[0], pos[1], vel[0], vel[1], head_width=0.05)
 
     plt.grid(True)
