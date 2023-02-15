9c78429cf31981cde2a857b5adfdf758d10f3791
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2023-02-15 19:09:28 +0100

    Add LUT computation for converting arc length to time

--- curves/cubic-bézier-2d.py
+++ curves/cubic-bézier-2d.py
@@ -61,7 +61,62 @@ def tangent(
     return velocity / np.linalg.norm(velocity)
 
 
-if __name__ == "__main__":
+def dist_to_time_lut(
+    ctrl_pts: npt.NDArray[np.float64],
+    num_samples: int = 1000
+) -> npt.NDArray[np.float64]:
+    """Compute a lookup table for converting distance to time."""
+    times = np.linspace(0, 1, num_samples)
+    samples = bezier(times[:, np.newaxis], ctrl_pts)
+
+    # Compute the distance between neighboring samples
+    distances = np.zeros(len(times))
+    distances[1:] = np.linalg.norm(np.diff(samples, axis=0), axis=1)  # first element should be 0
+    assert distances.shape == (len(times),), f"distances.shape: {distances.shape}"
+
+    cum_distances = np.cumsum(distances)
+
+    lut = np.hstack((cum_distances[:, np.newaxis], times[:, np.newaxis]))
+    assert lut.shape == (len(times), 2), f"lut.shape: {lut.shape}"
+    
+    # The array is by definition sorted, so look ups should be fast.
+    return lut
+
+
+def dist_to_time(lut: dict[np.float64, np.float64]):
+    """Convert a distance along the curve to its corresponding time.
+    
+    This is useful to uniformly sample the curve along its length, which the t parameter cannot do
+    if the curve has a non-zero second derivate, i.e. acceleration.
+
+    This solution is an example of arc-length parameterization. See
+    https://www.youtube.com/watch?v=aVwxzDHniEw for more details.
+    
+    """
+    raise NotImplementedError
+
+
+def plot_curve_with_tangents(ctrl_pts: npt.NDArray[np.float64], curve: npt.NDArray[np.float64]):
+    plt.plot(ctrl_pts[:, 0], ctrl_pts[:, 1], "o")
+    plt.plot(curve[:, 0], curve[:, 1])
+    
+    # Plot tangent at specific times
+    t_tans = np.linspace(0, 1, 5)
+    for t_tan in t_tans:
+        pos = bezier(t_tan, ctrl_pts)
+        vel = tangent(t_tan, ctrl_pts)
+        plt.arrow(pos[0], pos[1], vel[0], vel[1], head_width=0.05)
+
+    plt.grid(True)
+    plt.show()
+
+
+def uniformly_sample_curve(ctrl_pts: npt.NDArray[np.float64], curve: npt.NDArray[np.float64]):
+    lut = dist_to_time_lut(ctrl_pts)
+    print(lut)
+
+
+def main():
     control_points = np.array(
         [[0.0, 0.0],
         [0.5, 1.0],
@@ -75,18 +130,10 @@ if __name__ == "__main__":
     # Make t into a (num_time_points, 1) array
     t_reshaped = t[:, np.newaxis]
     
-    B = bezier(t_reshaped, control_points)
-    
+    curve = bezier(t_reshaped, control_points)
 
-    plt.plot(control_points[:, 0], control_points[:, 1], "o")
-    plt.plot(B[:, 0], B[:, 1])
-    
-    # Plot tangent at specific times
-    t_tans = np.linspace(0, 1, 5)
-    for t_tan in t_tans:
-        pos = bezier(t_tan, control_points)
-        vel = tangent(t_tan, control_points)
-        plt.arrow(pos[0], pos[1], vel[0], vel[1], head_width=0.05)
+    #plot_curve_with_tangents(control_points, curve)
+    uniformly_sample_curve(control_points, curve)
 
-    plt.grid(True)
-    plt.show()
+if __name__ == "__main__":
+    main()
