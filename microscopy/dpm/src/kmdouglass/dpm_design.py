d4f9833a98582c229559d2eeebd5064b6169abe0
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2023-03-22 14:34:07 +0100

    Add actual magnification calculation

--- microscopy/dpm/src/kmdouglass/dpm_design.py
+++ microscopy/dpm/src/kmdouglass/dpm_design.py
@@ -17,6 +17,10 @@ data = {
     "light_source.wavelength.units": Units.um,
     "grating.period": 1000/300,
     "grating.period.units": Units.um,
+    "lens_1.focal_length": 75,
+    "lens_1.focal_length.units": Units.mm,
+    "lens_2.focal_length": 300,
+    "lens_2.focal_length.units": Units.mm,
 }
 
 
@@ -37,17 +41,29 @@ def maximum_grating_period(data: dict[str, Any]) -> tuple[float, Units]:
     return period, data["light_source.wavelength.units"]
 
 
-def minimum_4f_magnification(data: dict[str, Any]) -> tuple[float, Units]:
+def minimum_4f_magnification(data: dict[str, Any]) -> float:
+    """Computes the minimum magnification of the 4f system for sufficient PSF/fringe sampling."""
+
     px_size = data["camera.pixel_size"] * data["camera.pixel_size.units"].value
     gr_period = data["grating.period"] * data["grating.period.units"].value
     wav = data["light_source.wavelength"] * data["light_source.wavelength.units"].value
 
     mag = 2 * px_size* (1 / gr_period + data["objective.numerical_aperture"] / wav / data["objective.magnification"])
 
-    return mag, None
+    return mag
+
+
+def actual_4f_magnification(data: dict[str, Any]) -> float:
+    """Computes the actual magnification of the 4f system."""
+
+    f1 = data["lens_1.focal_length"] * data["lens_1.focal_length.units"].value
+    f2 = data["lens_2.focal_length"] * data["lens_2.focal_length.units"].value
+
+    return -f2 / f1
 
 
 if __name__ == "__main__":
     print(resolution(data))
     print(maximum_grating_period(data))
     print(minimum_4f_magnification(data))
+    print(actual_4f_magnification(data))
