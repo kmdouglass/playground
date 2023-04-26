858e7d00799cd6fa626f4ed9f858f828f935900b
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2023-04-26 16:39:43 +0200

    Parameterize ray Newton-Raphson routine by distance from z-axis

--- newton-raphson/src/nr/main.py
+++ newton-raphson/src/nr/main.py
@@ -33,9 +33,6 @@ class FlatSurface(Surface):
     
     """
 
-    def __init__(self):
-        pass
-
     def sag(self, x: Float, y: Float) -> Float:
         """Return the z value (sag) of the surface at the given x and y coordinates.
         
@@ -52,9 +49,10 @@ class FlatSurface(Surface):
 
 
 def newton_raphson(
+    pos: npt.NDArray[Float],
     dir_cosines: npt.NDArray[Float], 
-    r0: npt.NDArray[Float],
     surface: Surface,
+    s_1: float = 0.0,
     tol=DEFAULT_TOL,
     max_iterations=MAX_ITERATIONS
 ) -> npt.NDArray[Float]:
@@ -64,19 +62,25 @@ def newton_raphson(
     intersection points. It returns the intersection points.
     
     """
-    r = r0.copy()
+    s_1 = Float(s_1)
+
+    # Parameterize the ray by distance s and find the intersection point with the z=0 plane
+    s = -pos[2] / dir_cosines[2]
+    curr_pos = np.array([pos[0] + s * dir_cosines[0], pos[1] + s * dir_cosines[1], 0], dtype=Float)
+
     for _ in range(max_iterations):
-        r = r - (r[2] - surface.sag(r[0], r[1])) / np.dot(surface.normal(r[0], r[1]), dir_cosines)
-        if np.linalg.norm(r - r0) < tol:
+        s = s - (dir_cosines[2] * s - surface.sag(curr_pos[0], curr_pos[1])) / np.dot(surface.normal(curr_pos[0], curr_pos[1]), dir_cosines)
+        curr_pos = np.array([curr_pos[0] + s * dir_cosines[0], curr_pos[1] + s * dir_cosines[1], s * dir_cosines[2]], dtype=Float)
+        if np.abs(s - s_1) < tol:
             break
-        r0 = r.copy()
-    return r
+        s_1 = s.copy()
+    return curr_pos
 
 
 def main():
-    dir_cosines = np.array([0.0, 0.0, 1.0], dtype=Float)
-    r0 = np.array([-1.0, 1.0, 0.0], dtype=Float)
+    pos = np.array([0.0, 0.0, -5.0], dtype=Float)  # Ray intersects the z axis at -5
+    dir_cosines = np.array([0.0, 0.7071, 0.7071], dtype=Float)  # Ray traveling at 45 degrees to the z axis
     surface = FlatSurface()
 
-    r = newton_raphson(dir_cosines, r0, surface)
+    r = newton_raphson(pos, dir_cosines, surface, s_1=-1.0)
     print(f"Intersection point: {r}")
