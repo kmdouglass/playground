a4fe51422d3c545ef537883435897f4b80e2de05
Kyle Douglass <kyle.m.douglass@gmail.com>
2024-07-02 09:48:12 +0200

    Add plugin dependencies

--- rust-plugin-dependencies/src/main.rs
+++ rust-plugin-dependencies/src/main.rs
@@ -1,8 +1,13 @@
 use std::any::Any;
+use std::collections::HashMap;
 
 trait Plugin {
     fn do_something(&self);
     fn as_any(&self) -> &dyn Any;
+    fn name(&self) -> &str;
+    fn dependencies(&self) -> Vec<String> {
+        Vec::new()
+    }
 }
 
 struct Plugin1 {}
@@ -10,7 +15,7 @@ struct Plugin1 {}
 struct Plugin2 {}
 
 struct System {
-    plugins: Vec<Box<dyn Plugin>>,
+    plugins: HashMap<String, Box<dyn Plugin>>,
 }
 
 impl Plugin for Plugin1 {
@@ -21,7 +26,22 @@ impl Plugin for Plugin1 {
     fn as_any(&self) -> &dyn Any {
         self
     }
+
+    fn name(&self) -> &str {
+        "Plugin1"
+    }
+
+    fn dependencies(&self) -> Vec<String> {
+        vec!["Plugin2".to_string()]
+    }
 }
+
+impl Plugin2 {
+    fn do_something_special(&self) {
+        println!("ConcretePlugin2 is very special.");
+    }
+}
+
 
 impl Plugin for Plugin2 {
     fn do_something(&self) {
@@ -31,6 +51,10 @@ impl Plugin for Plugin2 {
     fn as_any(&self) -> &dyn Any {
         self
     }
+
+    fn name(&self) -> &str {
+        "Plugin2"
+    }
 }
 
 fn main() {
@@ -38,16 +62,19 @@ fn main() {
     let plugin2 = Plugin2 {};
 
     let mut system = System {
-        plugins: Vec::new(),
+        plugins: HashMap::new(),
     };
 
-    system.plugins.push(Box::new(plugin1));
-    system.plugins.push(Box::new(plugin2));
+    system.plugins.insert(plugin1.name().to_owned(), Box::new(plugin1));
+    system.plugins.insert(plugin2.name().to_owned(), Box::new(plugin2));
 
-    for plugin in system.plugins.iter() {
+    for (_, plugin) in system.plugins.iter() {
         // Call do something only if the plugin is of type Plugin2
         if plugin.as_any().is::<Plugin2>() {
             plugin.do_something();
+
+            // Do something that only Plugin2 can do
+            plugin.as_any().downcast_ref::<Plugin2>().unwrap().do_something_special();
         }
     }
 }
