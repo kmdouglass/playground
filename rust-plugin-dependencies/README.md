ee60cd4ac33d4e18e17e407cf0c045809e47a9d6
Kyle Douglass <kyle.m.douglass@gmail.com>
2024-07-02 11:09:14 +0200

    Implement dependency resolution in Rust plugin example

--- rust-plugin-dependencies/README.md
+++ rust-plugin-dependencies/README.md
@@ -5,3 +5,5 @@ Prototype of a plugin system where plugins can depend on other plugins.
 ## Notes
 
 I use the `Any` trait to downcast the `Plugin` trait objects to their concrete types, which gives me access to specific methods implemented on those types. See the`Plugin2::do_something_special` method and its use in `main()`.
+
+The plugin system is implemented as a Hashmap, and Plugins provide their own dependencies. The dependencies are added to the HashMap if they don't already exist. Moving a Plugin and its dependencies into the HashMap must be done in the right order for the code to compile.
--- rust-plugin-dependencies/src/main.rs
+++ rust-plugin-dependencies/src/main.rs
@@ -5,7 +5,7 @@ trait Plugin {
     fn do_something(&self);
     fn as_any(&self) -> &dyn Any;
     fn name(&self) -> &str;
-    fn dependencies(&self) -> Vec<String> {
+    fn dependencies(&self) -> Vec<Box<dyn Plugin>> {
         Vec::new()
     }
 }
@@ -31,8 +31,8 @@ impl Plugin for Plugin1 {
         "Plugin1"
     }
 
-    fn dependencies(&self) -> Vec<String> {
-        vec!["Plugin2".to_string()]
+    fn dependencies(&self) -> Vec<Box<dyn Plugin>> {
+        vec![Box::new(Plugin2 {})]
     }
 }
 
@@ -59,14 +59,32 @@ impl Plugin for Plugin2 {
 
 fn main() {
     let plugin1 = Plugin1 {};
-    let plugin2 = Plugin2 {};
+    let input_plugins: Vec<Box<dyn Plugin>> = vec![Box::new(plugin1)];
 
+
+    // Check if Plugin1 has any dependencies
+    // If it does, add it to the hashmap
+    let mut plugins = HashMap::new();
+    for plugin in input_plugins {
+        // Get the dependencies of the plugin first; otherwise, we will lose the reference to the plugin
+        let mut dependencies: Vec<Box<dyn Plugin>> = plugin.dependencies();
+        
+        // Insert the plugin into the hashmap
+        plugins.entry(plugin.name().to_string()).or_insert(plugin);
+
+        // Insert the dependencies into the hashmap if they don't already exist
+        for dependency in dependencies.into_iter() {
+            plugins.entry(dependency.name().to_string()).or_insert(dependency);
+        }
+    }
+    
     let mut system = System {
-        plugins: HashMap::new(),
+        plugins: plugins,
     };
 
-    system.plugins.insert(plugin1.name().to_owned(), Box::new(plugin1));
-    system.plugins.insert(plugin2.name().to_owned(), Box::new(plugin2));
+    // Assert Plugin1 and Plugin2 are in the system
+    assert!(system.plugins.contains_key("Plugin1"));
+    assert!(system.plugins.contains_key("Plugin2"));
 
     for (_, plugin) in system.plugins.iter() {
         // Call do something only if the plugin is of type Plugin2
